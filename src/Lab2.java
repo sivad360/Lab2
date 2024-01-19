56d009197ea2fbf3793d0f2c893ff092ced25a93
Davis Tyler <davismartintyler@gmail.com>
2024-01-19 15:04:01 -0800

    fully functional

--- src/Lab2.java
+++ src/Lab2.java
@@ -11,11 +11,11 @@ import java.util.Scanner;
 
 public class Lab2 {
     public static void main(String[] args) {
-
+        while(true) {
         Menu Menu = new Menu();// this could be static?
 
         Menu.mainMenu(); // call main menu which gives option to quit or to start new game 
-        
+        }
     }
 
 }
@@ -24,7 +24,7 @@ class Game {
 
     public void newGame(){ // function which contains functions for one single game
         
-        int[] gameParameters = new int[5];
+        //int[] gameParameters = new int[5];
         Objects MyObjects = new Objects(); // initialize objects Cards, decks, etc...
         MyObjects.initialize();
 
@@ -34,15 +34,15 @@ class Game {
 
 }
 
-class Menu {
+class Menu { // contains menu functions which control user interactions outside the actual game funcitons
 
-    String[] mainMenuStrings = {"Welcome to _____ to continue please select an option","Options:"
+    String[] mainMenuStrings = {"Welcome to _____ to continue please select an option","Options:" // strings which contain menu strings to print out
     ,"Press (N) to start a new game", "Press (Q) to quit"};
     
     String[] gameOptionsMenuStrings = {"Welcome to _____ to continue please select an option","Options:"
     ,"Press (N) to start a new game", "Press (Q) to quit"};
 
-    public char genericMenu(String[] menuArray){
+    public char genericMenu(String[] menuArray){ // generic menu can be formatted by string array parameter.
 
         Scanner input = new Scanner(System.in);
 
@@ -60,12 +60,12 @@ class Menu {
 
     }
 
-    public void mainMenu() {
+    public void mainMenu() { // main entrance menu option to start new game or quit
         boolean cont = false;
 
         char inputChar = 'q';
 
-        while( cont == false) {
+        while( cont == false) { // check if input is valid 
 
             inputChar = genericMenu(mainMenuStrings);
         
@@ -77,12 +77,12 @@ class Menu {
 
         }
 
-        if (inputChar == 'n') {
+        if (inputChar == 'n') { // create new game and call game functions 
 
             Game Game = new Game();
             Game.newGame();
 
-        } else if (inputChar == 'q') {
+        } else if (inputChar == 'q') { 
 
             System. exit(0);
 
@@ -95,7 +95,7 @@ class Menu {
     //     return MyObjects;
     // }
 
-    static public void sub1Menu(Objects MyObjects){
+    static public void sub1Menu(Objects MyObjects){ // controls print out and shuffle functions calls will eventually contain options setup
 
         System.out.printf("%n%s%n%n","Ordered deck:");
         
@@ -105,18 +105,14 @@ class Menu {
         MyObjects.deck = MyObjects.shuffleDeck(MyObjects.deck);
 
         MyObjects.printDeck(MyObjects.deck);
-        
-
-        
-
 
     }
 
 }
 
-class Objects {
+class Objects { // class which contains current hands deck setups and discard piles.
 
-    class Card {
+    class Card { // generic card class contains int values for logic
         String face;
         String suit;
         int faceInt;
@@ -135,8 +131,7 @@ class Objects {
     //     //Parameters.players = givenParameters[1];
     // }
 
-    public void printDeck(Card[] deck){
-
+    public void printDeck(Card[] deck){ // print out current deck configuration
         Card current = new Card();
 
         for(int i = 0; i < deck.length; i++) {
@@ -148,19 +143,19 @@ class Objects {
         }
     }
 
-    public Card[] shuffleDeck(Card[] deck) {
+    public Card[] shuffleDeck(Card[] deck) { // shuffle deck which is passed to it. Could be for discard or for 
 
         Card[] shuffledDeck = new Card[52];
 
         Random rand = new Random();
 
-        for(int i = 0; i < deck.length; i ++) {
+        for(int i = 0; i < deck.length; i ++) { // take random card out of unshuffled deck and assign it to iterated index of shuffled deck
 
             int currentCardIndex;
 
             Card currentCard = null;
 
-            while(currentCard == null) {
+            while(currentCard == null) { // check that there is a valid card at the index in unshuffled deck if not generate new index
             
                 currentCardIndex = rand.nextInt(52);
                 currentCard = deck[currentCardIndex];
@@ -175,13 +170,13 @@ class Objects {
 
     }
 
-    Card hand[] = new Card[5];
+    Card hand[] = new Card[5]; // generic card array setup
 
     Card deck[] = new Card[52];
 
     Card discardDeck[] = new Card[52];
     
-    public void initialize() {
+    public void initialize() { // create all the cards
 
         String suits[] = new String[4];
         suits[0] = "Spades";
@@ -204,7 +199,7 @@ class Objects {
         faces[11] = "Queen";
         faces[12] = "King";
 
-        for(int i = 0; i< faces.length; i++) {
+        for(int i = 0; i< faces.length; i++) { // iterate and combine all card options into 52 unique cards.
 
             for(int j = 0; j< suits.length; j++) {
                 
